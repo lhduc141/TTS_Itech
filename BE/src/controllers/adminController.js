dc3f00ba410abcd9a97e2b99edddcdfeb8b55cb4
cyber141 <cyberzzz2k3@gmail.com>
2024-08-14 11:25:09 +0700

    Update: admin controller

--- BE/src/controllers/adminController.js
+++ BE/src/controllers/adminController.js
@@ -3,46 +3,79 @@ import initModels from "../models/init-models.js";
 import sequelize from "../config/database.js";
 let model = initModels(sequelize);
 
-export const putDetailInformation = async (req, res) => {}
-
-//
-export const putCarousel = async (req, res) => {
+export const putDetailInformation = async (req, res) => {
     try {
-        let { cpn_id } = req.params;
-        let {
+        const { cpn_id } = req.params;
+        const {
             cpn_name, cpn_sname, cpn_address,
             cpn_title, cpn_desc, cpn_copyright, cpn_content,
         } = req.body;
-        let updated_at = new Date(); // Generate the timestamp in the proper format
+        const update_at = new Date(); // Generate the timestamp in the proper format
 
-        let company = await model.information.findOne({
+        // Find the company by cpn_id
+        const company = await model.Information.findOne({
             where: { cpn_id },
         });
+        const data = {
+            cpn_name, cpn_sname, cpn_address,
+            cpn_title, cpn_desc, cpn_copyright, cpn_content, update_at,
+        };
 
         if (!company) {
             return responseData(res, "Fail", "Cannot find branch", 404);
-        } else {
-            let data = {
-                cpn_name, cpn_sname, cpn_address,
-                cpn_title, cpn_desc, cpn_copyright, cpn_content, updated_at,
-            };
+        }else {
             await company.update(data);
-            return responseData(res, "Success", data, 200); // Respond with success
+
         }
 
+        return responseData(res, "Success", data, 200);
     } catch (e) {
-        return responseData(res, "Error ...", e.message, 500); // Properly handle and return errors
+        return responseData(res, "Error", e.message, 500);
     }
+};
+
+//
+export const putCarousel = async (req, res) => {
+
+}
+
+export const postCarousel = async (req, res) => {
+
 }
 
 
-export const postCarousel = async (req, res) => {}
+//Feedback table
+export const putFeedback = async (req, res) => {
 
+}
 
-//
-export const putFeedback = async (req, res) => {}
+export const postFeedback = async (req, res) => {
 
-export const postFeedback = async (req, res) => {}
+    try {
+        const { fb_id } = req.params;
+        const {
+            fb_writer, fb_cpn, fb_content
+        } = req.body;
+        const update_at = new Date();
+
+        const company = await model.Information.findOne({
+            where: { fb_id , fb_writer, fb_cpn, fb_content},
+        });
+        let newCompany = {
+
+        }
+        if (company) {
+            responseData(res, "Fail", "feedback is available", 200);
+        }else {
+
+            await model.Information.create()
+        }
+
+        return responseData(res, "Success", data, 200);
+    } catch (e) {
+        return responseData(res, "Error", e.message, 500);
+    }
+}
 
 
 //
@@ -80,7 +113,6 @@ export const putFieldDetail = async (req, res) => {}
 
 export const postFieldDetail = async (req, res) => {}
 
-// export const  = async (req, res) => {}
 export const putProjects = async (req, res) => {}
 
 export const postProjects = async (req, res) => {}
@@ -102,28 +134,3 @@ export const getAdminDetails = async (req, res) => {
         responseData(res, "Error ...", e.message, 500);
     }
 }
-
-// try {
-//     let {id} = req.params;
-//
-//     let parent = await model.Parent.findOne({
-//         where: {ParentId: id},
-//         include: [{
-//             model: model.User, as: "user"
-//         }]
-//     });
-//     !parent ? responseData(res, "Fail", "Parent not found", 404) : null
-//
-//     let {address} = parent;
-//     let {name, phoneNumber, email} = parent.user;
-//     let data = {
-//         name,
-//         phoneNumber,
-//         address,
-//         email
-//     }
-//
-//     responseData(res, "Success", data, 200);
-// } catch (e) {
-//     responseData(res, "Error ...", e.message, 500);
-// }
